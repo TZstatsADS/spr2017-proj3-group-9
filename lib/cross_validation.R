04b40ace2c6b509968e3cd5b43c60991a2a9bc43
LiangbinChen <lc3190@columbia.edu>
2017-03-23 22:44:00 -0400

    Add files via upload

--- lib/cross_validation.R
+++ lib/cross_validation.R
@@ -35,6 +35,26 @@ cv.function <- function(X.train, y.train, d, K){
 
 
 
-
+rf.cv.function <- function(X.train, y.train,d,ntree=1000,mtry=sqrt(ncol(X.train)),K=5){
+  source("../lib/train.R")
+  source("../lib/test.R")
+  n <- length(y.train)
+  n.fold <- floor(n/K)
+  s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    train.data <- X.train[s != i,]
+    train.label <- y.train[s != i]
+    test.data <- X.train[s == i,]
+    test.label <- y.train[s == i]
+    
+    fit <- rf_train(train.data, train.label, ntree=ntree,mtry=mtry,node=d)
+    pred <- rf_test(fit, test.data)  
+    cv.error[i] <- mean(pred != test.label)  
+    
+  }			
+  return(mean(cv.error))
+}
 
 
